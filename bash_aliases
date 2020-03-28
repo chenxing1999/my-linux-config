4850168e1301b7e7e16b336ec3efeb8c80cba4ad
xing_Cin <xing@cinnamon.is>
2020-03-28 21:57:08 +0700

    more file

---
+++ bash_aliases
@@ -0,0 +1,1 @@
+alias gcd="cd $(git root)"
--- init.sh
+++ init.sh
@@ -9,9 +9,10 @@ sudo apt install build-essential cmake vim python3-dev
 
 echo "Copying source files..."
 #cp bashrc ~/.bashrc
-#cp bash_aliases ~/.bash_aliases
+cp bash_aliases ~/.bash_aliases
 ln -s vimrc ~/.vimrc
 ln -s ycm_extra_conf.py ~/.ycm_extra_conf
+ln -s tmux.conf ~/.tmux.conf
 
 echo "Setup Vim, YouCompleteMe"
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
@@ -19,5 +20,9 @@ vim +PluginInstall +qall
 cd ~/.vim/bundle/YouCompleteMe
 python3 install.py --clang-completer
 
+
+echo "Set up Git"
 git config --global core.autocrlf input
+git config --global --add alias.root '!pwd'
+
 cd $cur_dir
